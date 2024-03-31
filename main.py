2bb72816fa82d56b58508d6b04a574f65427afad
Teluguswagger <tharunteja2u@gmail.com>
2024-03-31 13:35:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import tweepy
-import os
-from json import dumps
+import time
 
-# Twitter API credentials
-bearer_key = os.environ.get("TWITTER_BEARER_KEY")
-api_key = os.environ.get("TWITTER_API_KEY")
-api_secret = os.environ.get("TWITTER_API_SECRET")
-access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+# Credentials
+api_key = "FENHX6h3SwfOIPDWlFsJOcQr3"
+api_secret = "ifvLyVXytfmy0fAvWnfR5Nl2n6NTNh1BTDYmdPP2tfD4qXA0k1"
+bearer_token = r"AAAAAAAAAAAAAAAAAAAAADKZsAEAAAAACfq5YP%2FtCKXSr%2BrPs6YpnIe%2FOXI%3Dn1KTggn5dQ6wlqOfvUXXlo3dZ9MKAdKVLg6km1wDWQGHTb6jRm"
+access_token = "1752015439629316096-MhqqlKKPnjgBhuOZQ4ffikMDOXedQI"
+access_token_secret = "8D7daj2XTgspeJIoS9fQBpjARVsQCYv9tB9nRrolfNPUe"
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+# Gainaing access and connecting to Twitter API using Credentials
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 
-# V2 Twitter API Authentication
-client = tweepy.Client(
-    bearer_key,
-    api_key,
-    api_secret,
-    access_token,
-    access_token_secret,
-    wait_on_rate_limit=True,
-)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
 
-# Upload image to Twitter. Replace 'filename' your image filename.
-media_id = api.media_upload(filename="twitter_image.jpg").media_id_string
-print(media_id)
 
-# Text to be Tweeted
-text = "#Pushpa2TheRule Updates ivvandi ra munda\n\n@MythriOfficial @PushpaMovie @SukumarWritings @imsarathchandra"
+# Bot searches for tweets containing certain keywords
+class MyStream(tweepy.StreamingClient):
 
-# Send Tweet with Text and media ID
-client.create_tweet(text=text, media_ids=[media_id])
-print("Tweeted!")
+    # This function gets called when a tweet passes the stream
+    def on_tweet(self, tweet):
+
+        # Retweeting the tweet
+        try:
+            # Retweet
+            client.retweet(tweet.id)
+
+            # Printing Tweet
+            print(tweet.text)
+
+            # Delay
+            #time.sleep(1)
+
+        except Exception as error:
+            # Error
+            print(error)
+
+
+# Creating Stream object
+stream = MyStream(bearer_token=bearer_token)
+
+# Adding terms to search rules
+rule = tweepy.StreamRule("( #Pushpa2TheRule) (-is:retweet -is:reply)") 
+stream.add_rules(rule, dry_run=True)
+
+# Starting stream
+stream.filter()
